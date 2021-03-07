8d70d9c1ce31a4c97e4a096c70ed94b14527b4a6
thiago oliveira vicente <thiago.vicente@tivit.com>
2021-03-07 17:16:07 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Components/Header'
+import Home from './Components/Home'
+
+
+import {
+  BrowserRouter as Router, 
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import Generos from './Components/Generos/Generos'
+import NovoGenero from './Components/Generos/NovoGenero'
+import EditarGenero from './Components/Generos/EditarGenero'
+import Series from './Components/Series/Series'
+import SerieNova from './Components/Series/SerieNova'
+import InfoSerie from './Components/Series/InfoSeries'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <Header />
+        <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/generos' exact component={Generos} />
+        <Route path='/genero/novo' exact component={NovoGenero} />
+        <Route path='/genero/:id' exact component={EditarGenero} />
+        <Route path='/series' exact component={Series} />
+        <Route path='/series/novo/' exact component={SerieNova} />
+        <Route path="/series/:id" exact component={InfoSerie} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
